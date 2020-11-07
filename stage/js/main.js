b9e147edb0c538aa5226a73b9c96ace1b46beb71
ahmed <ahmedelabasy662@gmail.com>
2020-11-08 01:49:17 +0200

    Full Screen Lesson

--- stage/js/main.js
+++ stage/js/main.js
@@ -8,4 +8,37 @@ $(function () {
         $(this).find(".fa-angle-right").toggleClass("down");
         $(this).next(".childs-list").slideToggle();
     });
+
+    // Toggle Full Screen
+    $(".toggle-fullscreen").on("click", function() {
+        $(this).toggleClass('fullscreen');
+        if($(this).hasClass("fullscreen")){
+            openFullScreen();
+        } else {
+            exitFullScreen();
+        }
+    })
 });
+
+    /* View in fullscreen */
+    var elem = document.documentElement;
+    function openFullScreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }
+    
+  /* Close fullscreen */
+    function exitFullScreen() {
+        if (document.exitFullscreen) {
+        document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+        }
+    }
